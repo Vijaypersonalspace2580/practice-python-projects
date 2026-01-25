aa1b71a27aa665604f67ad7fab05c885db97f078
VIJAY KUMAR REDDY CHINTHAM <vijaykumarreddychintham735@gmail.com>
2026-01-25 10:39:37 +0530

    Refactor password strength checker for user input loop

--- password_strength_checker.py
+++ password_strength_checker.py
@@ -1,33 +1,46 @@
 print('Welcome to password strength checker')
-p=input('enter your password=')
-length=len(p)
-if length>=6:
-    capital=0
-    small=0
-    special=0
-    number=0
+t='y'
+while t=='y' or t=='Y':
+    p=input('enter your password=')
+    length=len(p)
+    space=0
     for i in p:
-        if(i.isupper()==True):
-            capital+=1
-        elif(i.islower()==True):
-            small+=1
-        elif(i.isdigit()==True):
-            number+=1
-        else:
-            special+=1
-    if(capital>0 and small>0 and number>0 and special>0):
-        print('password is strong')
-    else:
+        if i==' ':
+            space+=1
+    if space>0:
         print('Invalid password')
-        if(capital==0):
-            print('There is no capital letter in password')
-        if(small==0):
-            print('There is no small letter in password')
-        if(number==0):
-            print('There is no number letter in password')
-        if(special==0):
-            print('There is no special character in password')
-else:
-    print('Invalid password')
-    print('password contain minimum 6 characters')
+        print('There is a space in password')
+        t=str(input('Do you want to enter another password?(y/n)='))
+    else:
+        if length>=6:
+            capital=0
+            small=0
+            special=0
+            number=0
+            for i in p:
+                if(i.isupper()==True):
+                    capital+=1
+                elif(i.islower()==True):
+                    small+=1
+                elif(i.isdigit()==True):
+                    number+=1
+                else:
+                    special+=1
+            if(capital>0 and small>0 and number>0 and special>0):
+                print('password is strong')
+                t='n'
+            else:
+                print('Invalid password')
+                if(capital==0):
+                    print('There is no capital letter in password')
+                if(small==0):
+                    print('There is no small letter in password')
+                if(number==0):
+                    print('There is no number in password')
+                if(special==0):
+                    print('There is no special character in password')
+                t = str(input('Do you want to enter another password?(y/n)='))
+        else:
+            print('Invalid password')
+            print('password contain minimum 6 characters')
 print('Thank you for visit password strength checker')
